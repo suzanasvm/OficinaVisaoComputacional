ebe766b41506484fa1593a722d2939f5799aecab
Suzana Viana Mota <suzana.svm@gmail.com>
2025-10-20 21:03:12 -0300

    Update gera_dados_sinteticos.py

--- scripts/gera_dados_sinteticos.py
+++ scripts/gera_dados_sinteticos.py
@@ -5,29 +5,13 @@ from faker import Faker
 def gerar_csv(qtd_itens, nome_arquivo="dados_fakes.csv"):
     fake = Faker('pt_BR')
     tipos_curso = ["Graduação", "Mestrado", "Doutorado"]
-    nomes_curso = [
-        "Engenharia Elétrica",
-        "Análise e Desenvolvimento de Sistemas",
-        "Medicina",
-        "Direito",
-        "Administração",
-        "Ciência da Computação",
-        "Engenharia Mecânica",
-        "Psicologia"
-    ]
 
     with open(nome_arquivo, mode='w', newline='', encoding='utf-8') as arquivo:
         escritor = csv.writer(arquivo)
-        escritor.writerow(["Nome Completo", "Tipo de Curso", "Nome do Curso", "Data de Validade"])
-
+        escritor.writerow(["nome_completo", "tipo_curso"])
         for _ in range(qtd_itens):
             nome = fake.name()
             tipo = random.choice(tipos_curso)
-            curso = random.choice(nomes_curso)
-            mes = random.randint(1, 12)
-            ano = random.randint(24, 35)
-            data_validade = f"{mes:02d}/{ano}"
-            escritor.writerow([nome, tipo, curso, data_validade])
 
     print(f"{qtd_itens} registros salvos em '{nome_arquivo}'")
 
